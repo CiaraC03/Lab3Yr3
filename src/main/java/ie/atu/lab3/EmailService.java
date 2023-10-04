ca5c505c8fbb72d938684c2121ac3da2c420a2b0
G00402357@atu.ie <G00402357@atu.ie>
2023-10-04 14:58:12 +0100

    Added both emailService and UserService

---
+++ src/main/java/ie/atu/lab3/EmailService.java
@@ -0,0 +1,12 @@
+package ie.atu.lab3;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    public void sendEmail(String to, String message)
+    {
+        //code to end
+    }
+
+}
--- src/main/java/ie/atu/lab3/UserService.java
+++ src/main/java/ie/atu/lab3/UserService.java
@@ -1,6 +1,25 @@
 package ie.atu.lab3;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private final EmailService emailService;
+
+    @Autowired
+    public UserService(EmailService emailService)
+    {
+        this.emailService = emailService;
+    }
+
+    public void registerUser(String username, String email)
+    {
+        //user registration logic
+
+        //send confirmation email
+        emailService.sendEmail(email, "Welcome to our platform!");
+    }
 
 
 
